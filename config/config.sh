e0322af512fcda0ac2a78e96464abcb73a946540
sperezmont <sergio.perez.mont@gmail.com>
2022-02-11 11:14:58 +0100

    config added + minor changes

---
+++ config/config.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+conda_path='/home/sergio/'
+env_name='yelmo_tools' # environment name
+py_version='3.7.3'       # python version
+
+echo 'Installing requirements ...'
+source ${conda_path}/anaconda3/etc/profile.d/conda.sh
+conda create -n ${env_name} python=${py_version}  
+conda activate ${env_name}
+pip install -r requirements.txt
+conda deactivate 
+echo 'Ready'
